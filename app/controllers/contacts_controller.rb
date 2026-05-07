c7977d6b51d7241adaa3c721c3a96b202a694db0
keitaroinoue6 <k25k.soccer@gmail.com>
2026-05-07 16:25:30 +0900

    問い合わせメール機能の作成

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,6 +6,7 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(contact_params)
     if @contact.valid?
+      ContactMailer.inquiry_notification(@contact).deliver_now
       redirect_to root_path, notice: "お問い合わせを受け付けました。2営業日以内にご返信いたします。"
     else
       render :new, status: :unprocessable_entity
